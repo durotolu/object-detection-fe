33f56bda78fce6726ac17684904a0c1e17279623
durotolu <durolawk@gmail.com>
2024-06-22 15:59:41 +0100

    added section

--- object-detection/app/page.tsx
+++ object-detection/app/page.tsx
@@ -53,6 +53,36 @@ export default function Home() {
           />
         </div>
       </header>
+      <div className="max-w-[832px] mt-4 pl-12 pr-16 pt-9 pb-10">
+        <h2 className="font-medium leading-6 text-xl mb-2">System check</h2>
+        <p className="font-normal leading-5 text-sm tracking-[.24px] mb-[30px] text-[#4A4A68]">
+          We utilize your camera image to ensure fairness for all participants,
+          and we also employ both your camera and microphone for a video
+          questions where you will be prompted to record a response using your
+          camera or webcam, so it's essential to verify that your camera and
+          microphone are functioning correctly and that you have a stable
+          internet connection. To do this, please position yourself in front of
+          your camera, ensuring that your entire face is clearly visible on the
+          screen. This includes your forehead, eyes, ears, nose, and lips. You
+          can initiate a 5-second recording of yourself by clicking the button
+          below.
+        </p>
+        <div className="mb-10">
+          <div className="bg-[#F5F3FF] w-[91px] h=[71px] rounded-[10px] px-6 pt-[9px] pb-[12px] relative">
+            <div className="bg-[#755AE2] w-4 h-4 rounded-[30px] absolute top-[2px] right-[3px]"></div>
+            <Image
+              // className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
+              src={webcam}
+              alt="Webcam"
+              className="m-auto mb-1"
+              // width={180}
+              // height={37}
+              // priority
+            />
+            <div className="font-normal leading-3 text-[10px] tracking-[.24px]">Webcam</div>
+          </div>
+        </div>
+      </div>
     </main>
   );
 }
