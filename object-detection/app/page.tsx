6f35870c61342b8133a8a55e3608dbbf8685b0ad
durotolu <durolawk@gmail.com>
2024-06-22 19:31:48 +0100

    cleaned up

--- object-detection/app/page.tsx
+++ object-detection/app/page.tsx
@@ -13,16 +13,7 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center">
       <header className="flex w-full justify-between pt-4 pb-2 px-14 bg-[#FFFFFF] shadow-[3px_5px_20px_10px_#0000000A]">
         <div className="flex">
-          {/* <div> */}
-          <Image
-            // className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-            src={logo}
-            alt="Logo"
-            // width={180}
-            // height={37}
-            // priority
-          />
-          {/* </div> */}
+          <Image src={logo} alt="Logo" />
           <div className="ml-3 m-auto">
             <h1 className="font-dm-sans text-[20px] font-medium leading-[26.04px] tracking-[-0.24px] text-left">
               Frontend developer
@@ -35,26 +26,16 @@ export default function Home() {
         <div className="flex items-center">
           <div className="rounded-lg bg-[#ECE8FF] flex p-3 items-center text-[#755AE2] font-['DM_Sans']">
             <Image
-              // className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
               className="mr-[10px]"
               src={clock}
               alt="Clock"
               width={24}
               height={24}
-              // priority
             />
             <time className="mr-[4px] font-bold text-lg leading-6">29:10</time>
             <span className="font-medium text-sm leading-3">time left</span>
           </div>
-          <Image
-            // className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-            src={eye}
-            alt="Check"
-            className="ml-[10px]"
-            // width={180}
-            // height={37}
-            // priority
-          />
+          <Image src={eye} alt="Check" className="ml-[10px]" />
         </div>
       </header>
       <div className="max-w-[832px] mt-6 pl-12 pr-16 pt-9 pb-10 shadow-[3px_5px_20px_10px_#0000000A] rounded-[20px]">
--- object-detection/components/Check.tsx
+++ object-detection/components/Check.tsx
@@ -1,9 +1,4 @@
 import Image from "next/image";
-// import logo from "./icons/logo.svg";
-// import clock from "./icons/clock.svg";
-// import eye from "./icons/eye.svg";
-// import webcam from "./icons/webcam.svg";
-// import ObjectDetection from "../components/ObjectDetection";
 
 export default function Check({
   icon,
@@ -16,13 +11,9 @@ export default function Check({
     <div className="bg-[#F5F3FF] w-[91px] h-[71px] rounded-[10px] text-center pt-[9px] pb-[12px] relative mr-4">
       <div className="bg-[#755AE2] w-4 h-4 rounded-[30px] absolute top-[2px] right-[3px]"></div>
       <Image
-        // className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
         src={icon}
         alt={label}
         className="m-auto mb-1"
-        // width={180}
-        // height={37}
-        // priority
       />
       <div className="font-normal leading-3 text-[10px] tracking-[.24px]">
         {label}
